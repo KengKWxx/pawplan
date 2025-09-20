93415489f2ae50f5b82adcdbf6f35de2942a1c2b
KengKWxx <Tontrakool.j@ku.th>
2025-09-20 20:06:30 +0700

    ข้อความอธิบายการเปลี่ยนแปลง

---
+++ android/app/src/main/kotlin/com/example/pawplan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pawplan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "PawPlan Login",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text("ยังไม่ได้เชื่อม Firebase")),
+                );
+              },
+              child: const Text("Login"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
